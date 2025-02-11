bdb9c57f54c658f26aa2414be7fd38e2f01cb9bc
livboer <107176062+livboer@users.noreply.github.com>
2025-02-11 11:29:35 +0200

    Feb. 11 - Language button

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const languageSelect = document.querySelector('.Languages-btns');
 
-
-// Get the query parameter from the URL
+languageSelect.addEventListener('change', () => {
+    if (languageSelect.value === 'pt') {
+        window.location.href = 'pt.html'; // Redirect to Portuguese page
+    } else if (languageSelect.value === 'en') {
+        window.location.href = 'index.html'; // Redirect to English page (if needed)
+    }
+});
